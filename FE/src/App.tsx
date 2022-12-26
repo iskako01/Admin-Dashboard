6c0ba700a27581a2c22f8b0f597ee925a7656a1a
iskako01 <85966697+iskako01@users.noreply.github.com>
2022-12-26 22:02:56 +0100

    Added: Geography page

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -8,6 +8,7 @@ import DefaultLayout from "./layout";
 import Products from "./scenes/products";
 import Customers from "./scenes/customers";
 import Transactions from "./scenes/transactions";
+import Geography from "./scenes/geography";
 
 function App() {
   const mode = useAppSelector((state) => state.global.mode);
@@ -25,6 +26,7 @@ function App() {
             <Route path="/products" element={<Products />} />
             <Route path="/customers" element={<Customers />} />
             <Route path="/transactions" element={<Transactions />} />
+            <Route path="/geography" element={<Geography />} />
           </Route>
         </Routes>
       </ThemeProvider>
