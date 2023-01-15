6c412a289cb172b04f2e5be9cc47acb605f7ad6b
iskako01 <85966697+iskako01@users.noreply.github.com>
2023-01-15 11:36:11 +0100

    Added: Dashboard route

--- BE/controllers/general.js
+++ BE/controllers/general.js
@@ -1,10 +1,60 @@
 import User from "../models/User.js";
+import OverallState from "../models/OverallState.js";
+import Transactions from "../models/Transactions.js";
 
 export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
+
     res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getDashboardStats = async (req, res) => {
+  try {
+    // Hardcoded values
+    const currentMonth = "November";
+    const currentYear = 2021;
+    const currentDay = "2021-11-15";
+
+    // Transactions
+
+    const transactions = await Transactions.find()
+      .limit(50)
+      .sort({ createdOn: -1 });
+
+    // Overall Stats
+    const overallStat = await OverallState.find({ year: currentYear });
+
+    const {
+      totalCustomers,
+      yesrlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+    } = overallStat[0];
+
+    const thisMonthStats = overallStat[0].monthlyData.find(({ month }) => {
+      return month === currentMonth;
+    });
+
+    const todayStats = overallStat[0].dailyData.find(({ date }) => {
+      return date === currentDay;
+    });
+
+    res.status(200).json({
+      totalCustomers,
+      yesrlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+      thisMonthStats,
+      todayStats,
+      transactions,
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
